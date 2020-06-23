9c6c48fe12ecae17025b5cb078987a9485838fdc
ChebilFedi <fchebil93@gmail.com>
2020-06-23 11:12:52 +0100

    second commit

--- src/Store.js
+++ src/Store.js
@@ -1,3 +1,17 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer  from './Reducers'
-export default createStore(rootReducer);
+
+const logger = store => next => action => {
+    console.log('action object: ', action)
+    
+    
+    next(action)
+
+    let x = store.getState()
+    console.log(x)
+}
+
+export default createStore(
+    rootReducer,
+    compose(applyMiddleware(logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    );
